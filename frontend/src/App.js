ae1113c4afc72ecfe9cb7ec9f1141cf539785cdd
Sebastian <sebbecker@gmx.net>
2018-11-13 14:39:51 +0000

    more stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import Form from './form';
+import Home from './home';
+import ChatRoom from './chatroom'
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        Guess Vu!
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/chatroom" component={ChatRoom} />
+        </div>
+      </Router>
     );
   }
 }
