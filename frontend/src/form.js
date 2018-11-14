4a7fb916eae27a8c41a19d92bfad9b34de23764c
Ibra <ibra@MacBook-Pro-6.local>
2018-11-14 17:00:12 +0000

    Form component is now a stateless functional component

--- frontend/src/form.js
+++ frontend/src/form.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-class Form extends Component {
-  handleSubmit() {
+const Form = (props) => { 
+  const handleSubmit = () => {
     const fakeName = document.getElementById('fakeName').value;
     const realName = document.getElementById('realName').value;
-    this.props.setNames(fakeName, realName);
-    this.props.history.push('/chatroom');
+    props.setNames(fakeName, realName);
+    props.history.push('/chatroom');
   }
 
-  render() {
-    return (
-      <div class="Form">
-        <input name="fakeName" id="fakeName" placeholder="Fake Name" />
-        <input name="realName" id="realName" placeholder="Real Name" />
-        <button type="submit" onClick={() => this.handleSubmit()}>
-          Play
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div class="Form">
+      <input name="fakeName" id="fakeName" placeholder="Fake Name" />
+      <input name="realName" id="realName" placeholder="Real Name" />
+      <button type="submit" onClick={() => handleSubmit()}>
+        Play
+      </button>
+    </div>
+  );
 }
 
 export default withRouter(Form);
