177218f5c3c7520fb5c91add1bb430526cbc24dd
ckvuby <41907439+ckvuby@users.noreply.github.com>
2018-11-14 23:25:05 +0000

    Update routes.js
    removed unnecessary console log

--- routes.js
+++ routes.js
@@ -2,8 +2,6 @@ const Users = require('./models/users');
 
 var users = new Users();
 
-console.log("Here again");
-
 module.exports = (app) => {
     app.post('/api/user', (req, res) => {
         const result = users.add(req.body.fakeName, req.body.realName);
