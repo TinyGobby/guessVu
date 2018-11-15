b8ce76b36303115ec927cbd416168c68f56f5e58
Patrick Harris <patricknpharris@gmail.com>
2018-11-15 12:35:00 +0000

    real names now shown on chatroom

--- frontend/src/chatroom.js
+++ frontend/src/chatroom.js
@@ -1,63 +1,79 @@
 import React, { Component } from 'react';
-import DisplayMessages from './displayMessages'
+import DisplayMessages from './displayMessages';
 import socket from './index.js';
+import ShowRealNames from './realNames';
 
 class ChatRoom extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       input: '',
-      messages: []
-    }
+      messages: [],
+      realNames: []
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e){
-    this.setState({ input: e.target.value })
+  handleChange(e) {
+    this.setState({ input: e.target.value });
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     const that = this;
     let sentObj = {
       msg: this.state.input,
       fakeName: this.props.user.fakeName,
       userId: this.props.user.id
-    }
+    };
     e.preventDefault();
     socket.emit('inputMessage', sentObj);
     this.setState({
       input: ''
-    })
+    });
   }
 
   componentDidMount() {
     const that = this;
     // allows user to see updated version of message list
     // when joining room
-    socket.emit('retrieveMessages')
+    socket.emit('retrieveMessages');
+    // socket.emit('getRealNames');
 
-    socket.on('listOfMessages', function (data) {
+    socket.on('listOfMessages', function(data) {
       that.setState({
         messages: data
-      })
-    })
+      });
+    });
+
+    socket.on('listOfRealNames', function(data) {
+      that.setState({
+        realNames: data
+      });
+    });
   }
 
-render() {
-  return (
-        <div className="ChatRoom">
-          <h1 className="ChatRoom-title">Welcome {this.props.user.fakeName}</h1>
-          <form onSubmit={this.handleSubmit}>
-            <input name="message" className="MessageForm" value={this.state.input} onChange={this.handleChange} />
-            <button type='submit'>Submit!</button>
-          </form>
-          <div className="DisplayedMessages">
-            <DisplayMessages messages={this.state.messages} />
-          </div>
+  render() {
+    return (
+      <div className="ChatRoom">
+        <h1 className="ChatRoom-title">Welcome {this.props.user.fakeName}</h1>
+        <div>
+          <ShowRealNames realNames = {this.state.realNames} />
         </div>
-  )
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="message"
+            className="MessageForm"
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit!</button>
+        </form>
+        <div className="DisplayedMessages">
+          <DisplayMessages messages={this.state.messages} />
+        </div>
+      </div>
+    );
   }
 }
 
